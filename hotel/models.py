b24b4c2b50d9a3f59c61aebad3281b15ee30c156
alberta-f <delyasalakhova@gmail.com>
2025-03-20 00:24:59 +0300

    Добавила новое поле в HotelRoom + добавила сортировку

--- hotel/models.py
+++ hotel/models.py
@@ -3,10 +3,12 @@ from django.db import models
 # Create your models here.
 class HotelRoom(models.Model):
     price_per_night = models.IntegerField()
+    created_at = models.DateField(auto_now_add=True)
 
 
 class Booking(models.Model):
-    room = models.ForeignKey(HotelRoom, related_name='bookings', 
+    room = models.ForeignKey(HotelRoom, 
+                             related_name='bookings', 
                              on_delete=models.CASCADE)
     start_date = models.DateField()
     end_date = models.DateField()
--- hotel/views.py
+++ hotel/views.py
@@ -7,6 +7,16 @@ from .serializers import HotelRoomSerializer, BookingSerializer
 
 
 # Create your views here.
+def apply_sorting(queryset, sort_by, order):
+    if sort_by in ('price_per_night', 'created_at'):
+
+        if order == 'desc':
+            sort_by = f'-{sort_by}'
+        
+        return queryset.order_by(sort_by)
+    return queryset
+
+
 class BaseCRUDView(APIView):
     model = None
     serializer_class = None
@@ -20,21 +30,30 @@ class BaseCRUDView(APIView):
             return None
         
 
-    def get(self, request, pk=None):
-        if pk is not None:
-            instance = self.get_object(pk)
+    def get_one(self, request, pk):
+        instance = self.get_object(pk)
 
-            if instance is None:
-                return Response({'error': 'Not found'}, 
-                                status=status.HTTP_404_NOT_FOUND)
-            
+        if instance:
             serializer = self.serializer_class(instance)
+
+            return Response(serializer.data, 
+                        status=status.HTTP_200_OK)
+
+        return Response({'error': 'Not found'}, 
+                        status=status.HTTP_404_NOT_FOUND)
+    
+
+    def get(self, request):
+        sort_by = request.query_params.get('sort_by')
+        order = request.query_params.get('order', 'asc')
+
+        queryset = self.model.objects.all()
+        queryset = apply_sorting(queryset, sort_by, order)
+
+        serializer = self.serializer_class(queryset, many=True)
         
-        else:
-            queryset = self.model.objects.all()
-            serializer = self.serializer_class(queryset, many=True)
-        
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, 
+                        status=status.HTTP_200_OK)
     
 
     def put(self, request, pk):
@@ -42,12 +61,14 @@ class BaseCRUDView(APIView):
 
         if instance:
             serializer = self.serializer_class(instance, data=request.data)
+
             if serializer.is_valid():
                 serializer.save()
 
                 return Response(serializer.data)
         
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, 
+                        status=status.HTTP_400_BAD_REQUEST)
     
 
     def post(self, request):
@@ -56,9 +77,11 @@ class BaseCRUDView(APIView):
         if serializer.is_valid():
             serializer.save()
 
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, 
+                            status=status.HTTP_201_CREATED)
         
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, 
+                        status=status.HTTP_400_BAD_REQUEST)
     
 
     def delete(self, request, pk):
@@ -68,7 +91,8 @@ class BaseCRUDView(APIView):
             instance.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
 
-        return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'error': 'Not found'}, 
+                        status=status.HTTP_404_NOT_FOUND)
 
 
 class HotelRoomCRUDView(BaseCRUDView):
