d6dec330744a4676f63f35e79440cf9333bff965
alberta-f <delyasalakhova@gmail.com>
2025-03-19 15:56:13 +0300

    Добавила модели HotelRoom, Booking

---
+++ hotel/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class HotelRoom(models.Model):
+    price_per_night = models.IntegerField()
+
+
+class Booking(models.Model):
+    room_id = models.ForeignKey(HotelRoom, related_name='bookings', on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    
