a0d0faf0ea75845ae12358de26c3e87cf54239ce
alberta-f <delyasalakhova@gmail.com>
2025-03-19 23:52:07 +0300

    Добавила провевку на занятость номера

--- hotel/serializers.py
+++ hotel/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import HotelRoom, Booking
-
+from .booking_service import is_room_available
 
 class BaseSerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,5 +23,10 @@ class BookingSerializer(BaseSerializer):
             raise serializers.ValidationError(
                 'Дата начала должна быть раньше даты окончания'
             )
-
+        
+        if not is_room_available(data['room'], data['start_date'], data['end_date']):
+            raise serializers.ValidationError(
+                'Номер занят в указанные даты'
+            )
         return data
+    
